2fa37dd23d320d7b4c9b2483bb0a9336918b2f0a
madpeh <48161600+madpeh@users.noreply.github.com>
2021-11-21 16:43:51 +0100

    Initial version of tigase server

---
+++ server.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+function usage() {
+    echo "Usage: $0 {start|stop|restart|clean|logs|stats|pgadmin}"
+    echo "       start: (build and) start the server"
+    echo "       stop: stop the server"
+    echo "       restart: clean and start"
+    echo "       clean: stop the server and remove all docker data"
+    echo "       logs [SERVICE]: print logs from SERVICE (if SERVICE is not provided, then print logs from all services)"
+    echo "       stats: print stats from all services"
+    echo "       pgadmin: start pgadmin"
+    exit 1
+}
+
+function start() {
+    # docker-compose build --parallel && \
+    docker-compose up --detach db && \
+    docker-compose up --detach xmpp-server-0 && \
+    docker-compose up --detach xmpp-server-1 && \
+    docker-compose up --detach xmpp-server-2 && \
+    docker-compose up --detach xmpp-server-3 && \
+    docker-compose up --detach load-balancer
+}
+
+function stop() {
+    docker-compose stop
+}
+
+function restart() {
+    clean && start
+}
+
+function clean() {
+    docker-compose down --volumes --remove-orphans --rmi all && \
+    docker system prune --all --volumes
+}
+
+function logs () {
+    docker-compose logs --follow --tail="all" "${@}" 
+}
+
+function stats() {
+    docker stats
+}
+
+function pgadmin() {
+    docker-compose up --detach pgadmin
+}
+
+case "${1}" in
+    start)
+        start
+        ;;
+
+    stop)
+        stop
+        ;;
+
+    restart)
+        restart
+        ;;
+
+    clean)
+        clean
+        ;;
+
+    logs)
+        logs "${@:2}"
+        ;;
+
+    stats)
+        stats
+        ;;
+
+    pgadmin)
+        pgadmin
+        ;;
+
+    *)
+        usage
+        ;;
+esac
