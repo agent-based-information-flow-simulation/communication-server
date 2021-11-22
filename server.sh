caf577148990decd423664b030aea3e7ed81b418
madpeh <48161600+madpeh@users.noreply.github.com>
2021-11-22 20:42:37 +0100

    Add tsung benchmark

--- server.sh
+++ server.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 function usage() {
-    echo "Usage: $0 {start|stop|restart|clean|logs|stats|pgadmin}"
+    echo "Usage: $0 {start|stop|restart|clean|logs|stats|pgadmin|benchmark}"
     echo "       start: (build and) start the server"
     echo "       stop: stop the server"
     echo "       restart: clean and start"
     echo "       clean: stop the server and remove all docker data"
     echo "       logs [SERVICE]: print logs from SERVICE (if SERVICE is not provided, then print logs from all services)"
     echo "       stats: print stats from all services"
-    echo "       pgadmin: start pgadmin"
+    echo "       pgadmin {start|stop}: start/stop pgadmin"
+    echo "       benchmark {start|stop}: start/stop benchmark"
     exit 1
 }
 
 function start() {
-    # docker-compose build --parallel && \
     docker-compose up --detach db && \
     docker-compose up --detach xmpp-server-0 && \
     docker-compose up --detach xmpp-server-1 && \
@@ -44,7 +44,34 @@ function stats() {
 }
 
 function pgadmin() {
-    docker-compose up --detach pgadmin
+    case "${1}" in
+        start)
+            docker-compose up --detach pgadmin && \
+            echo "pgadmin is running on http://localhost:5433"
+            ;;
+        stop)
+            docker-compose stop pgadmin
+            ;;
+        *)
+            usage
+            ;;
+    esac
+}
+
+function benchmark() {
+    case "${1}" in
+        start)
+            docker-compose build tsung-benchmark && \
+            docker-compose up --detach tsung-benchmark && \
+            echo "benchmark is running on http://localhost:8091"
+            ;;
+        stop)
+            docker-compose stop tsung-benchmark
+            ;;
+        *)
+            usage
+            ;;
+    esac
 }
 
 case "${1}" in
@@ -74,6 +101,10 @@ case "${1}" in
 
     pgadmin)
         pgadmin
+        ;;
+
+    benchmark)
+        benchmark "${2}"
         ;;
 
     *)
