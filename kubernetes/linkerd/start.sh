50853909634c715fc4d0f9f2e9405fd80029a103
madpeh <48161600+madpeh@users.noreply.github.com>
2021-11-26 19:34:08 +0100

    Fix startup script

--- kubernetes/linkerd/start.sh
+++ kubernetes/linkerd/start.sh
@@ -2,7 +2,7 @@
 linkerd check --pre && \
 linkerd install | kubectl apply -f - && \
 linkerd check && \
-kubectl get -n default deploy -o yaml | linkerd inject - | kubectl apply -f - && \
+kubectl get -n default statefulsets -o yaml | linkerd inject - | kubectl apply -f - && \
 linkerd -n default check --proxy && \
 linkerd viz install | kubectl apply -f - && \
 linkerd check
