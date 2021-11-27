8157d4d975424cd0dc7b0c084f50d5cd2f1abbb4
madpeh <48161600+madpeh@users.noreply.github.com>
2021-11-27 17:57:21 +0100

    Add sleep to wait for db

--- docker-swarm/xmpp-server/scripts/run.sh
+++ docker-swarm/xmpp-server/scripts/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sleep 60
 /tigase/scripts/tigase.sh install-schema ./etc/tigase.conf \
     -T postgresql \
     -D tigasedb \
@@ -7,7 +8,7 @@
     -P admin \
     -R postgres \
     -A postgres \
-    -J admin@localhost \
+    -J admin@agents-sim.xyz \
     -N admin
 /usr/bin/python3.9 /tigase/scripts/register_in_load_balancer.py `cat /etc/hostname`
 /tigase/scripts/tigase.sh run ./etc/tigase.conf
