45a1b98cc250ba063857929fe8bb49ae5c33812b
madpeh <48161600+madpeh@users.noreply.github.com>
2021-11-27 20:11:51 +0100

    Add publish script to gitignore

--- docker-swarm/_publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker build ./xmpp-server -t madpeh/xmpp-server-docker-swarm && \
-docker push madpeh/xmpp-server-docker-swarm
-
-docker build ./load-balancer -t madpeh/load-balancer-docker-swarm && \
-docker push madpeh/load-balancer-docker-swarm
-
-docker build ./tsung-benchmark -t madpeh/tsung-benchmark-docker-swarm && \
-docker push madpeh/tsung-benchmark-docker-swarm
