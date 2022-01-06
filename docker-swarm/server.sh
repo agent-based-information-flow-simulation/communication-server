3bb063935818bc42768f909437f77cb547c1a3d5
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-06 13:39:26 +0100

    Update server script

--- docker-swarm/server.sh
+++ docker-swarm/server.sh
@@ -1,55 +1,100 @@
 #!/bin/bash
 
 function usage() {
-    echo "Usage: $0 {init|start|scale|stop|restart|clean|stats|benchmark}"
+    echo "Usage: $0 {init|join|network|start|scale|stop|clean|stats|publish|benchmark}"
     echo "       init: initialize the swarm cluster"
-    echo "       start [N=1]: start the server with N instances of xmpp servers"
+    echo "       join TOKEN IP:PORT: join the swarm cluster"
+    echo "       network (REQUIRES SWARM MODE): create shared networks for the swarm mode"
+    echo "       start [-n <N=1>: N xmpp servers] [-d: dev mode (REQUIRES SWARM MODE) [-p: publish]]: start the server"
     echo "       scale N: scale the server to N instances of xmpp servers"
     echo "       stop: stop the server"
-    echo "       restart [N=1]: clean and start with N instances of xmpp servers"
     echo "       clean: stop the server and remove all docker data"
     echo "       stats: print stats from all services"
-    echo "       benchmark {start|stop}: start/stop benchmark"
+    echo "       publish [-d: dev mode (REQUIRES SWARM MODE)]: publish the images to a registry"
+    echo "       benchmark {start|stop|publish}: start/stop/publish benchmark"
     exit 1
 }
 
 function init() {
     if docker swarm init; then
-        echo "ok"
-        echo ""
-        echo "-------------------------------------------------------------------------------"
-        echo "Point the server's domain (agents-sim.xyz) to the IP address of the master node"
-        echo "For instance, add the IP address of the master node to /etc/hosts"
-        echo "W.X.Y.Z agents-sim.xyz"
-        echo "However, this step depends on the deployment method"
-        echo "-------------------------------------------------------------------------------"
+        echo "swarm mode initialized"
+    else
+        echo "failed to initialize swarm mode"
+    fi
+}
+
+function join() {
+    if [ -z "$1" ]; then
+        echo "missing token"
+        usage
+    fi
+    if [ -z "$2" ]; then
+        echo "missing address"
+        usage
+    fi
+    if docker swarm join --token "$1" "$2"; then
+        echo "swarm mode joined"
+    else
+        echo "failed to join swarm mode"
+    fi
+}
+
+function network() {
+    if docker network create --driver overlay --attachable sre-cs; then
+        echo "[simulation run environment <-> communication server] created"
+    else
+        echo "[simulation run environment <-> communication server] failed to create"
     fi
 }
 
 function start() {
-    if docker stack deploy -c ./docker-compose.yml agents-sim; then
-        if [ ! -z "${1}" ]; then scale "${1}"; fi
+    DEV=0
+    N=1
+    PUBLISH=0
+    while getopts dn:p opt; do
+        case $opt in
+            n) N=$OPTARG ;;
+            d) DEV=1 ;;
+            p) PUBLISH=1 ;;
+            *) usage ;;
+        esac
+    done
+
+    if [ "$DEV" -eq "1" ]; then
+        COMPOSE_FILE=docker-compose.dev.swarm.yml
+        if [ "$PUBLISH" -eq "1" ]; then publish -d; fi
+    else
+        COMPOSE_FILE=docker-compose.swarm.yml
+    fi
+
+    if docker stack deploy -c ./"$COMPOSE_FILE" cs; then
+        if [ ! -z "${1}" ]; then scale "$N"; fi
         echo "XMPP can be accessed on port 5222"
         echo "Admin panel can be accessed on port 8080"
     else
+        echo ""
         echo "failed to start the server"
+        echo ""
         echo "if you see the following error:"
         echo "failed to create service X: Error response from daemon: network Y not found"
         echo "then restart docker daemon (i.e. sudo systemctl restart docker) and run ./server.sh clean"
+        echo ""
+        echo "if you see the following error:"
+        echo "network XXX is declared as external, but could not be found"
+        echo "run the script with the network option to create the required networks"
     fi
 }
 
 function scale() {
-    if [ -z "${1}" ]; then usage; fi
-    docker service scale agents-sim_xmpp-server="${1}"
+    if [ -z "${1}" ]; then
+        echo "missing number of instances"
+        usage
+    fi
+    docker service scale cs_xmpp-server="${1}"
 }
 
 function stop() {
-    docker stack rm agents-sim
-}
-
-function restart() {
-    clean && init && start "${1}"
+    docker stack rm cs
 }
 
 function clean() {
@@ -62,60 +107,66 @@ function stats() {
     docker stats
 }
 
+function publish() {
+    local OPTIND
+    DEV=0
+    while getopts d opt; do
+        case $opt in
+            d) DEV=1 ;;
+            *) usage ;;
+        esac
+    done
+
+    if [ "$DEV" -eq "1" ]; then
+        echo "creating local registry..." 
+        docker service create --name registry --publish published=5000,target=5000 registry:2
+        docker-compose -f docker-compose.dev.swarm.yml build --parallel
+        docker-compose -f docker-compose.dev.swarm.yml push
+    else
+        docker-compose -f docker-compose.swarm.yml build --parallel
+        docker-compose -f docker-compose.swarm.yml push
+    fi
+}
+
 function benchmark() {
     case "${1}" in
-        start)
-            docker run \
+        start) docker run \
                 -d \
                 --rm \
                 --network host \
                 --name tsung-benchmark \
-                madpeh/tsung-benchmark-docker-swarm && \
-            echo "benchmark is running on http://localhost:8091"
+                madpeh/communication-server-tsung-benchmark && \
+                echo "benchmark is running on http://localhost:8091"
             ;;
-        stop)
-            docker stop tsung-benchmark
-            ;;
-        *)
-            usage
-            ;;
+
+        stop) docker stop tsung-benchmark ;;
+
+        publish) ./tsung-benchmark/publish.sh ;;
+
+        *) usage ;;
     esac
 }
 
 case "${1}" in
-    init)
-        init
-        ;;
+    init) init ;;
 
-    start)
-        start "${2}"
-        ;;
+    join) join "${2}" "${3}" ;;
 
-    scale)
-        scale "${2}"
-        ;;
+    network) network ;;
 
-    stop)
-        stop
-        ;;
+    start) start "${@:2}" ;;
 
-    restart)
-        restart "${2}"
-        ;;
+    scale) scale "${2}" ;;
 
-    clean)
-        clean
-        ;;
+    stop) stop ;;
 
-    stats)
-        stats
-        ;;
+    clean) clean ;;
 
-    benchmark)
-        benchmark "${2}"
-        ;;
+    stats) stats ;;
 
-    *)
-        usage
-        ;;
+    publish) publish "${@:2}" ;;
+
+    benchmark) benchmark "${2}" ;;
+
+    *) usage ;;
 esac
