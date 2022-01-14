967a7c70bc76538e90d01692868b0e3b6f80ae0b
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-14 23:09:44 +0100

    Update wait_for_x scripts

--- xmpp-server/startup_scripts/wait_for_entrypoint.sh
+++ xmpp-server/startup_scripts/wait_for_entrypoint.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-while ! nc -z entrypoint 5555; do sleep 1; done;
+# env variables:
+# - WAIT_FOR_ENTRYPOINT_ADDRESS
+
+IFS=':' read -ra ADDRESS_SPLIT <<< "${WAIT_FOR_ENTRYPOINT_ADDRESS}"
+if [ "${#ADDRESS_SPLIT[@]}" -ne "2" ]; then
+    echo "Expected argument format: HOST:PORT"
+    echo "Unexpected argument: ${ADDRESS}"
+    echo "Split (on ':' delimiter) into:"
+    for PART in "${ADDRESS_SPLIT[@]}"; do
+        echo "${PART}"
+    done
+    exit 1
+fi
+HOST="${ADDRESS_SPLIT[0]}"
+PORT="${ADDRESS_SPLIT[1]}"
+
+while ! nc -z "${HOST}" "${PORT}"; do sleep 1; done;
 echo "Entrypoint is up"
